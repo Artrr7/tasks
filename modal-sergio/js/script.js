83acd0ade9424068850a165b8bb46e23c6710513
Arthur <Sistemas1@softhair.com.br>
2025-11-03 11:02:52 -0300

    treino

--- modal-sergio/js/script.js
+++ modal-sergio/js/script.js
@@ -1,16 +1,35 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const modal = new bootstrap.Modal(document.getElementById('myModal'));
+// modal aparece assim q a tela e carregada
+document.addEventListener("DOMContentLoaded", function() {
+  const modal = new bootstrap.Modal(document.getElementById('myModal'))
   modal.show();
-
-  setTimeout(function () {
-    modal.hide()
-    console.log("depois de 12s")
-  }, 12000)
 });
 
+// close sumir e aparece
+document.getElementById('btnM').style.display = 'none'
+setTimeout(function () {
+  document.getElementById('btnM').style.display = 'inline'
+}, 12000)
+
+// segundos regressivo
+contRegress = setInterval(function() {
+  const cronometro = document.getElementById('contRegress').innerHTML
+  const soma = parseInt(cronometro) - 1
+  if(soma===0){
+    document.getElementById('contRegress').innerHTML= "Voce ja pode fechar"
+    pararContagem()
+  }else{
+    document.getElementById('contRegress').innerHTML= soma
+  }
+}, 1000)
+
+function pararContagem(){
+  clearInterval(contRegress)
+}
+
+// barra progresso
 let barra = document.getElementById("bp")
 let carga = 0
-let intBarra = setInterval(function(){
+let intBarra = setInterval(function() {
   barra.style.width = carga + "%"
   carga++
-},115)
+}, 114)
---
+++ trainne/js/script.js
@@ -0,0 +1,59 @@
+const frutas = ['banana', 'maçã', 'pera']
+for (let fruta of frutas) {
+    console.log(frutas)
+}
+
+
+const pessoa = { nome: 'Arthur', idade: 20 }
+for (let chave in pessoa) {
+    console.log(chave, pessoa[chave])
+}
+
+
+const fruits = ['banana', 'maçã', 'pera']
+for (let i in frutas) {
+    console.log(i, frutas[i])
+}
+
+
+const bolos = ['cenoura', 'chocolate', 'moramgo']
+for (let bolo of bolos) {
+    console.log(bolo.toUpperCase())
+}
+
+
+document.getElementById("btn").addEventListener("click", function () {
+    const nome = document.getElementById("nome").value
+    alert('ola, $`(nome)!')
+})
+
+
+
+
+let resultado;
+document.getElementById("calcular").addEventListener("click", function () {
+
+    switch (operacao) {
+        case "1":
+            resultado = num1 + num2;
+            break;
+        case "2":
+            resultado = num1 - num2;
+            break;
+        case "3":
+            resultado = num1 * num2;
+            break;
+        case "4":
+            if (num2 === 0) {
+                resultado = "Erro: divisão por zero";
+            } else {
+                resultado = num1 / num2;
+            }
+            break;
+        default:
+            resultado = "Operação inválida";
+    }
+
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+    alert("resultado")
+});
