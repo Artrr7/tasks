db0d4496a92fd759abb91980cfc0155abbacc372
Arthur <Sistemas1@softhair.com.br>
2025-11-03 11:02:39 -0300

    treino

---
+++ foreachAndMap/js/script.js
@@ -0,0 +1,18 @@
+const myArrayMap = [5, 6, 7, 8, 9];
+
+const myArrayForEach = [
+  { id: 1, name: 'arthur' },
+  { id: 2, name: 'fagner' },
+  { id: 3, name: 'kaio' }
+];
+
+const result = myArrayMap.map((value, index) => 2);
+console.log(result);
+
+const result2 = myArrayForEach.forEach((value) => console.log(value.name));
+console.log(result2);
+
+
+const users = [
+   
+]
